5f430f38035ba3e7282a43d08d8765519b435675
lorenzopenati76 <lorenzo.penati76@gmail.com>
2017-05-19 22:52:45 +0100

    added test cases

--- CA4Working/test_simple.py
+++ CA4Working/test_simple.py
@@ -6,7 +6,7 @@ from simple import get_commits, read_file
 class TestCommits(unittest.TestCase):
 
     def setUp(self):
-        self.data = read_file('changes_python.log')
+        self.data = read_file('Data.txt')
 
     def test_number_of_lines(self):
         self.assertEqual(5255, len(self.data))
@@ -19,6 +19,17 @@ class TestCommits(unittest.TestCase):
         commits = get_commits(self.data)
         self.assertEqual('Thomas', commits[0]['author'])
         self.assertEqual('r1551925', commits[0]['revision'])
+        self.assertEqual('2015-11-27', commits[0]['date'])
+        self.assertEqual('16:57:44', commits[0]['time'])
+        self.assertEqual('Fri', commits[0]['day'])      
+        
+    def test_last_commit(self):
+        commits = get_commits(self.data)
+        self.assertEqual('Thomas', commits[421]['author'])
+        self.assertEqual('r1491146', commits[421]['revision'])
+        self.assertEqual('2015-07-13', commits[421]['date'])
+        self.assertEqual('09:21:48', commits[421]['time'])
+        self.assertEqual('Mon', commits[421]['day'])         
 
 if __name__ == '__main__':
     unittest.main()
